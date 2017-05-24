924d970fe212afcb19c58e652db07a2f24dd373e
Nieminen Liisa P <linili@da5-cs-b221-28.pc.helsinki.fi>
2017-05-24 18:07:03 +0300

    lisauksia

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,37 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	//  View::make('home.html');
+        echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+        View::make('helloworld.html');
+      // echo 'Hello World!!!';
+    }
+    
+    public static function rekisterointi() {
+        echo 'Täällä rekisteröidytään';
+    }
+    
+    public static function kirjautuminen() {
+        echo 'Täällä kirjaudutaan';
+        
+    }
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+    
+    public static function aanestys_list() {
+        View::make('suunnitelmat/aanestys_list.html');
+    }
+    
+    public static function aanestys_tiedot() {
+        View::make('suunnitelmat/aanestys_tiedot.html');
+    }
+    
+    public static function aanestys_muokkaa() {
+        View::make('suunnitelmat/aanestys_muokkaa.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,27 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/rekisterointi', function() {
+  HelloWorldController::rekisterointi();
+  });
+  
+   $routes->get('/kirjautuminen', function() {
+  HelloWorldController::kirjautuminen();
+  });
+  
+  $routes->get('/login', function() {
+  HelloWorldController::login();
+  });
+  
+   $routes->get('/aanestys_list', function() {
+  HelloWorldController::aanestys_list();
+  });
+  
+   $routes->get('/aanestys_tiedot', function() {
+  HelloWorldController::aanestys_tiedot();
+  });
+  
+   $routes->get('/aanestys_muokkaa', function() {
+  HelloWorldController::aanestys_muokkaa();
+  });
